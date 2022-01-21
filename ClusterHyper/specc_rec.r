9eb5b364213cf3bb77cbffeb934e58a6e4035b40
Mariana Fortes <48294820+MarianaFortes@users.noreply.github.com>
2022-01-21 19:44:24 -0300

    Update specc_rec.r

--- ClusterHyper/specc_rec.r
+++ ClusterHyper/specc_rec.r
@@ -115,7 +115,7 @@ specc_rec <- function(
   cluster <- candidate_cluster$cluster[1]
   pred_results_candidate <- pred_results[pred_results$cluster == cluster,]
   
-  #===== Within-cluster recommendation of a combination of hyperparameters =====
+  #===== Intra-cluster recommendation of a combination of hyperparameters =====
   
   # Calculates the mode of each hyperparameter in the candidate cluster, and
   # selects the combination with the values of each mode (not yet used)
